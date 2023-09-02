96cca8fa80fd4c0c36120d5d81846bd5426214b8
dhash-deakin <139202942+dhash-deakin@users.noreply.github.com>
2023-09-02 11:07:05 +1000

    Create client.py

---
+++ server/client.py
@@ -0,0 +1,29 @@
+import socket
+
+# Create a socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect(('127.0.0.1', 12345))
+
+# User input for username and password
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+# Send username to the server
+client_socket.send(username.encode())
+
+# Receive server's response
+response = client_socket.recv(1024).decode()
+print(response)
+
+if "username accepted" in response.lower():
+    # Send password to the server
+    client_socket.send(password.encode())
+
+    # Receive authentication result
+    authentication_result = client_socket.recv(1024).decode()
+    print(authentication_result)
+
+# Close the socket
+client_socket.close()
